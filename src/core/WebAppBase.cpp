8278e85728891879f353f1f698e248b46b220cd6
Nick Diego Yamane <nickdiego@igalia.com>
2018-11-16 10:30:08 -0400

    [agl][qtless] Drop signal/slots in favor of WebPageObserver
    - This patch replaces Qt-specific signals/slots usage in WAM
    by an Observer pattern based approach. Such mechanism is used
    to route WebView events between WebPage and WebApp objects.
    - Assuming all events coming from underlying WebView implementation
    are called in the WAM main loop's thread, this observer-based approach
    should be equivalent to Qt-based signal/slot previous implementation.
    
    [SPEC-1912] Qt-less WAM: drop QObject + Q_SLOT + Q_SIGNALS
    
    Signed-off-by: Nick Diego Yamane <nickdiego@igalia.com>

--- src/core/WebAppBase.cpp
+++ src/core/WebAppBase.cpp
@@ -220,7 +220,7 @@ void WebAppBase::closeAppInternal()
 
 void WebAppBase::attach(WebPageBase* page)
 {
-    // connect to the signals of the WebBridge
+    // start observing the WebPage
     // parse up the ApplicationDescription
     if (d->m_page)
         detach();
@@ -229,9 +229,6 @@ void WebAppBase::attach(WebPageBase* page)
     d->m_page->createPalmSystem(this);
 
     observe(d->m_page);
-    connect(d->m_page, SIGNAL(webPageUrlChanged()), this, SLOT(webPageUrlChangedSlot()));
-    connect(d->m_page, SIGNAL(webPageLoadFinished()), this, SLOT(webPageLoadFinishedSlot()));
-    connect(d->m_page, SIGNAL(webPageLoadFailed(int)), this, SLOT(webPageLoadFailedSlot(int)));
     d->createActivity();
 }
 
@@ -239,7 +236,6 @@ WebPageBase* WebAppBase::detach(void)
 {
     WebPageBase* p = d->m_page;
 
-    disconnect(d->m_page, 0, this, 0);
     unobserve(d->m_page);
 
     d->m_page = 0;
@@ -304,7 +300,7 @@ void WebAppBase::relaunch(const QString& args, const QString& launchingAppId)
     }
 }
 
-void WebAppBase::webPageLoadFinishedSlot()
+void WebAppBase::webPageLoadFinished()
 {
     doPendingRelaunch();
 }
@@ -323,7 +319,7 @@ void WebAppBase::doPendingRelaunch()
     }
 }
 
-void WebAppBase::webPageClosePageRequestedSlot()
+void WebAppBase::webPageClosePageRequested()
 {
     LOG_INFO(MSGID_WINDOW_CLOSED_JS, 2, PMLOGKS("APP_ID", qPrintable(appId())), PMLOGKFV("PID", "%d", page()->getWebProcessPID()), "");
     WebAppManager::instance()->closeApp(appId().toStdString());
@@ -349,11 +345,6 @@ void WebAppBase::showWindow()
     // Set the accessibility after the application launched
     // because the chromium can generate huge amount of AXEvent during app loading.
     setUseAccessibility(WebAppManager::instance()->isAccessibilityEnabled());
-}
-
-void WebAppBase::showWindowSlot()
-{
-    showWindow();
 }
 
 void WebAppBase::setAppDescription(ApplicationDescription* appDesc)
@@ -462,11 +453,6 @@ void WebAppBase::setUiSize(int width, int height) {
     WebAppManager::instance()->setUiSize(width, height);
 }
 
-void WebAppBase::webPageUrlChangedSlot()
-{
-    d->m_url = d->m_page->url().toString();
-}
-
 void WebAppBase::setPreferredLanguages(QString language)
 {
     if (!d->m_page)
@@ -503,14 +489,31 @@ void WebAppBase::setUseAccessibility(bool enabled)
 
 void WebAppBase::executeCloseCallback()
 {
-    connect(d->m_page, SIGNAL(closeCallbackExecuted()),this, SLOT(closeWebAppSlot()));
-    connect(d->m_page, SIGNAL(timeoutExecuteCloseCallback()),this, SLOT(closeWebAppSlot()));
-    connect(d->m_page, SIGNAL(closingAppProcessDidCrashed()),this, SLOT(closeWebAppSlot()));
     page()->executeCloseCallback(forceClose());
     LOG_INFO(MSGID_EXECUTE_CLOSECALLBACK, 2, PMLOGKS("APP_ID", qPrintable(appId())), PMLOGKFV("PID", "%d", page()->getWebProcessPID()), "");
 }
 
-void WebAppBase::closeWebAppSlot()
+void WebAppBase::closeCallbackExecuted()
+{
+	closeWebApp();
+}
+
+void WebAppBase::timeoutExecuteCloseCallback()
+{
+	closeWebApp();
+}
+
+void WebAppBase::closingAppProcessDidCrashed()
+{
+	closeWebApp();
+}
+
+void WebAppBase::didDispatchUnload()
+{
+	closeWebApp();
+}
+
+void WebAppBase::closeWebApp()
 {
     LOG_INFO(MSGID_CLEANRESOURCE_COMPLETED, 2, PMLOGKS("APP_ID", qPrintable(appId())), PMLOGKFV("PID", "%d", page()->getWebProcessPID()), "closeCallback/about:blank is DONE");
     WebAppManager::instance()->removeClosingAppList(appId());
@@ -524,8 +527,6 @@ void WebAppBase::closeWebAppSlot()
 
 void WebAppBase::dispatchUnload()
 {
-    connect(d->m_page, SIGNAL(didDispatchUnload()),this, SLOT(closeWebAppSlot()));
-    connect(d->m_page, SIGNAL(closingAppProcessDidCrashed()),this, SLOT(closeWebAppSlot()));
     page()->cleanResources();
 }
 
