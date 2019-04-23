22fd52536e7e7aa12635066a92197132951bb870
Antia Puentes <apuentes@igalia.com>
2019-04-23 18:30:35 +0200

    [agl] use appid instead of appname in tap_shortcut
    [SPEC-2154] Invisible layer created for WAM applications

--- src/agl/WebRuntimeAGL.cpp
+++ src/agl/WebRuntimeAGL.cpp
@@ -292,10 +292,7 @@ bool WebAppLauncherRuntime::init_wm() {
   std::function< void(json_object*) > h_syncdraw =
       [this](json_object* object) {
     LOG_DEBUG("Got Event_SyncDraw");
-    json_object* obj = json_object_new_object();
-    json_object_object_add(obj, this->m_wm->kKeyDrawingName,
-                           json_object_new_string(this->m_role.c_str()));
-    this->m_wm->endDraw(obj);
+    this->m_wm->endDraw(this->m_role.c_str());
   };
 
   std::function< void(json_object*) > h_flushdraw= [](json_object* object) {
@@ -320,26 +317,8 @@ bool WebAppLauncherRuntime::init_hs() {
   }
 
   std::function< void(json_object*) > handler = [this] (json_object* object) {
-    json_object *val;
-
-    if (json_object_object_get_ex(object, "application_name", &val)) {
-      const char *name = json_object_get_string(val);
-
-      LOG_DEBUG("Event_TapShortcut <%s>", name);
-
-      if (strcmp(name, this->m_name.c_str()) == 0) {
-        // check app exist and re-launch if needed
-        LOG_DEBUG("Activesurface %s ", this->m_role.c_str());
-
-        json_object *obj = json_object_new_object();
-        json_object_object_add(obj, this->m_wm->kKeyDrawingName,
-                               json_object_new_string(this->m_role.c_str()));
-        json_object_object_add(obj, this->m_wm->kKeyDrawingArea,
-                               json_object_new_string("normal.full"));
-
-        this->m_wm->activateWindow(obj);
-      }
-    }
+    LOG_DEBUG("Activesurface %s ", this->m_role.c_str());
+    this->m_wm->activateWindow(this->m_role.c_str(), "normal.full");
   };
   m_hs->set_event_handler(LibHomeScreen::Event_TapShortcut, handler);
 
@@ -407,26 +386,14 @@ void WebAppLauncherRuntime::setup_surface (int id)
   std::string sid = std::to_string(id);
 
   // This surface is mine, register pair app_name and ivi id.
-  json_object *obj = json_object_new_object();
-  json_object_object_add(obj, m_wm->kKeyDrawingName,
-                         json_object_new_string(m_role.c_str()));
-  json_object_object_add(obj, m_wm->kKeyIviId,
-                         json_object_new_string(sid.c_str()));
-
-  LOG_DEBUG("requestSurfaceXDG(%s,%s)", m_role.c_str(), sid.c_str());
-  m_wm->requestSurfaceXDG(obj);
+  LOG_DEBUG("requestSurfaceXDG(%s,%d)", m_role.c_str(), id);
+  m_wm->requestSurfaceXDG(this->m_role.c_str(), id);
 
   if (m_pending_create) {
     // Recovering 1st time tap_shortcut is dropped because
     // the application has not been run yet (1st time launch)
     m_pending_create = false;
-
-    json_object *obj = json_object_new_object();
-    json_object_object_add(obj, m_wm->kKeyDrawingName,
-                           json_object_new_string(m_role.c_str()));
-    json_object_object_add(obj, m_wm->kKeyDrawingArea,
-                           json_object_new_string("normal.full"));
-    m_wm->activateWindow(obj);
+    m_wm->activateWindow(this->m_role.c_str(), "normal.full");
   }
 }
 
