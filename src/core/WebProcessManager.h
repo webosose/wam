nickdiego/webosose-wam

[agl][qtless] Get rid of QString usage in WebProcessManager
- This patch removes QString usage from WebProcessManager implementation.
To do that it also modifies classes that have some relationship with it,
such as WebAppManager and WebAppManagerService.
- Also, add some FIXME comments to mark points for next steps on removing
QString references from WAM.

[SPEC-1911] Qt-less WAM: replace qt basic data structures

Signed-off-by: Nick Diego Yamane <nickdiego@igalia.com>

src/core/WebProcessManager.h
@@ -21,8 +21,6 @@
 #include <unordered_map>
 #include <vector>
 
-#include <QString>
-
 class ApplicationDescription;
 class WebPageBase;
 class WebAppBase;
@@ -38,17 +36,17 @@ public:
 
     uint32_t getWebProcessProxyID(const ApplicationDescription* desc) const;
     uint32_t getWebProcessProxyID(uint32_t pid) const;
-    QString getWebProcessMemSize(uint32_t pid) const; //change name from webProcessSize(uint32_t pid)
+    std::string getWebProcessMemSize(uint32_t pid) const; //change name from webProcessSize(uint32_t pid)
     void killWebProcess(uint32_t pid);
     void requestKillWebProcess(uint32_t pid);
     bool webProcessInfoMapReady();
-    void setWebProcessCacheProperty(const Json::Value &object, QString key); //change name from setWebProcessProperty()
+    void setWebProcessCacheProperty(const Json::Value &object, const std::string& key); //change name from setWebProcessProperty()
     void readWebProcessPolicy(); //chane name from setWebProcessEnvironment()
-    QString getProcessKey(const ApplicationDescription* desc) const; //change name from getKey()
+    std::string getProcessKey(const ApplicationDescription* desc) const; //change name from getKey()
 
     virtual Json::Value getWebProcessProfiling() = 0;
     virtual uint32_t getWebProcessPID(const WebAppBase* app) const = 0;
-    virtual void deleteStorageData(const QString& identifier) = 0;
+    virtual void deleteStorageData(const std::string& identifier) = 0;
     virtual uint32_t getInitialWebViewProxyID() const = 0;
     virtual void clearBrowsingData(const int removeBrowsingDataMask) = 0;
     virtual int maskForBrowsingDataType(const char* type) = 0;
@@ -58,7 +56,7 @@ public:
 protected:
     std::list<const WebAppBase*> runningApps();
     std::list<const WebAppBase*> runningApps(uint32_t pid);
-    WebAppBase* findAppById(const QString& appId);
+    WebAppBase* findAppById(const std::string& appId);
     WebAppBase* getContainerApp();
 
 protected:
@@ -87,11 +85,11 @@ protected:
         uint32_t codeCacheSize;
         bool requestKill;
     };
-    std::unordered_map<QString, WebProcessInfo> m_webProcessInfoMap;
+    std::unordered_map<std::string, WebProcessInfo> m_webProcessInfoMap;
 
     uint32_t m_maximumNumberOfProcesses;
-    std::vector<QString> m_webProcessGroupAppIDList;
-    std::vector<QString> m_webProcessGroupTrustLevelList;
+    std::vector<std::string> m_webProcessGroupAppIDList;
+    std::vector<std::string> m_webProcessGroupTrustLevelList;
 };
 
 #endif /* WEBPROCESSMANAGER_H */

src/platform/webengine/BlinkWebProcessManager.cpp
@@ -21,7 +21,6 @@
 #include <set>
 
 #include <json/value.h>
-#include <QString>
 
 #include "WebPageBlink.h"
 #include "WebAppBase.h"
@@ -43,14 +42,14 @@ Json::Value BlinkWebProcessManager::getWebProcessProfiling()
     uint32_t pid;
     std::set<uint32_t> processIdList;
 
-    std::unordered_multimap<uint32_t, QString> runningAppsMap;
+    std::unordered_multimap<uint32_t, std::string> runningAppsMap;
     std::list<const WebAppBase*> running = runningApps();
     for (auto it = running.begin(); it != running.end(); ++it) {
-        WebAppBase* app = findAppById((*it)->appId());
+        auto appid = (*it)->appId().toStdString(); //TODO port appId to std::string
+        WebAppBase* app = findAppById(appid);
         pid = getWebProcessPID(app);
         processIdList.insert(pid);
-
-        runningAppsMap.emplace(pid, app->appId());
+        runningAppsMap.emplace(pid, app->appId().toStdString());
     }
 
     WebAppBase* containerApp = getContainerApp();
@@ -58,21 +57,21 @@ Json::Value BlinkWebProcessManager::getWebProcessProfiling()
         pid = getWebProcessPID(containerApp);
         processIdList.insert(pid);
 
-        runningAppsMap.emplace(pid, containerApp->appId());
+        runningAppsMap.emplace(pid, containerApp->appId().toStdString());
     }
 
     for (uint32_t pid : processIdList) {
         Json::Value processObject(Json::objectValue);
         Json::Value appArray(Json::arrayValue);
 
-        processObject["pid"] = QString::number(pid).toStdString();
-        processObject["webProcessSize"] = getWebProcessMemSize(pid).toStdString();
+        processObject["pid"] = std::to_string(pid);
+        processObject["webProcessSize"] = getWebProcessMemSize(pid);
         //starfish-surface is note used on Blink
         processObject["tileSize"] = 0;
         auto processes = runningAppsMap.equal_range(pid);
         for (auto p = processes.first; p != processes.second; ++p) {
             Json::Value appObject(Json::objectValue);
-            appObject["id"] = p->second.toStdString();
+            appObject["id"] = p->second;
             appArray.append(appObject);
         }
         processObject["runningApps"] = appArray;
@@ -84,7 +83,7 @@ Json::Value BlinkWebProcessManager::getWebProcessProfiling()
     return std::move(reply);
 }
 
-void BlinkWebProcessManager::deleteStorageData(const QString& identifier)
+void BlinkWebProcessManager::deleteStorageData(const std::string& identifier)
 {
     std::list<const WebAppBase*> runningAppList = runningApps();
     if (!runningAppList.empty()) {
@@ -100,7 +99,7 @@ void BlinkWebProcessManager::deleteStorageData(const QString& identifier)
 
     BlinkWebView* webview = new BlinkWebView();
     if (webview) {
-        webview->DeleteWebStorages(identifier.toStdString());
+        webview->DeleteWebStorages(identifier);
         delete webview;
     }
 }

webosose/wam

src/core/WebProcessManager.h
@@ -21,8 +21,6 @@
 #include <map>
 #include <vector>
 
-#include <QString>
-
 class ApplicationDescription;
 class WebPageBase;
 class WebAppBase;
@@ -38,17 +36,17 @@ public:
 
     uint32_t getWebProcessProxyID(const ApplicationDescription* desc) const;
     uint32_t getWebProcessProxyID(uint32_t pid) const;
-    QString getWebProcessMemSize(uint32_t pid) const; //change name from webProcessSize(uint32_t pid)
+    std::string getWebProcessMemSize(uint32_t pid) const; //change name from webProcessSize(uint32_t pid)
     void killWebProcess(uint32_t pid);
     void requestKillWebProcess(uint32_t pid);
     bool webProcessInfoMapReady();
-    void setWebProcessCacheProperty(const Json::Value &object, QString key); //change name from setWebProcessProperty()
+    void setWebProcessCacheProperty(const Json::Value &object, const std::string& key); //change name from setWebProcessProperty()
     void readWebProcessPolicy(); //chane name from setWebProcessEnvironment()
-    QString getProcessKey(const ApplicationDescription* desc) const; //change name from getKey()
+    std::string getProcessKey(const ApplicationDescription* desc) const; //change name from getKey()
 
     virtual Json::Value getWebProcessProfiling() = 0;
     virtual uint32_t getWebProcessPID(const WebAppBase* app) const = 0;
-    virtual void deleteStorageData(const QString& identifier) = 0;
+    virtual void deleteStorageData(const std::string& identifier) = 0;
     virtual uint32_t getInitialWebViewProxyID() const = 0;
     virtual void clearBrowsingData(const int removeBrowsingDataMask) = 0;
     virtual int maskForBrowsingDataType(const char* type) = 0;
@@ -58,7 +56,7 @@ public:
 protected:
     std::list<const WebAppBase*> runningApps();
     std::list<const WebAppBase*> runningApps(uint32_t pid);
-    WebAppBase* findAppById(const QString& appId);
+    WebAppBase* findAppById(const std::string& appId);
     WebAppBase* getContainerApp();
 
 protected:
@@ -87,11 +85,11 @@ protected:
         uint32_t codeCacheSize;
         bool requestKill;
     };
-    std::map<QString, WebProcessInfo> m_webProcessInfoMap;
+    std::map<std::string, WebProcessInfo> m_webProcessInfoMap;
 
     uint32_t m_maximumNumberOfProcesses;
-    std::vector<QString> m_webProcessGroupAppIDList;
-    std::vector<QString> m_webProcessGroupTrustLevelList;
+    std::vector<std::string> m_webProcessGroupAppIDList;
+    std::vector<std::string> m_webProcessGroupTrustLevelList;
 };
 
 #endif /* WEBPROCESSMANAGER_H */

src/platform/webengine/BlinkWebProcessManager.cpp
@@ -21,7 +21,6 @@
 #include <set>
 
 #include <json/value.h>
-#include <QString>
 
 #include "WebPageBlink.h"
 #include "WebAppBase.h"
@@ -43,14 +42,14 @@ Json::Value BlinkWebProcessManager::getWebProcessProfiling()
     uint32_t pid;
     std::set<uint32_t> processIdList;
 
-    std::multimap<uint32_t, QString> runningAppsMap;
+    std::multimap<uint32_t, std::string> runningAppsMap;
     std::list<const WebAppBase*> running = runningApps();
     for (auto it = running.begin(); it != running.end(); ++it) {
-        WebAppBase* app = findAppById((*it)->appId());
+        auto appid = (*it)->appId().toStdString(); //TODO port appId to std::string
+        WebAppBase* app = findAppById(appid);
         pid = getWebProcessPID(app);
         processIdList.insert(pid);
-
-        runningAppsMap.emplace(pid, app->appId());
+        runningAppsMap.emplace(pid, app->appId().toStdString());
     }
 
     WebAppBase* containerApp = getContainerApp();
@@ -58,21 +57,21 @@ Json::Value BlinkWebProcessManager::getWebProcessProfiling()
         pid = getWebProcessPID(containerApp);
         processIdList.insert(pid);
 
-        runningAppsMap.emplace(pid, containerApp->appId());
+        runningAppsMap.emplace(pid, containerApp->appId().toStdString());
     }
 
     for (uint32_t pid : processIdList) {
         Json::Value processObject(Json::objectValue);
         Json::Value appArray(Json::arrayValue);
 
-        processObject["pid"] = QString::number(pid).toStdString();
-        processObject["webProcessSize"] = getWebProcessMemSize(pid).toStdString();
+        processObject["pid"] = std::to_string(pid);
+        processObject["webProcessSize"] = getWebProcessMemSize(pid);
         //starfish-surface is note used on Blink
         processObject["tileSize"] = 0;
         auto processes = runningAppsMap.equal_range(pid);
         for (auto p = processes.first; p != processes.second; ++p) {
             Json::Value appObject(Json::objectValue);
-            appObject["id"] = p->second.toStdString();
+            appObject["id"] = p->second;
             appArray.append(appObject);
         }
         processObject["runningApps"] = appArray;
@@ -84,7 +83,7 @@ Json::Value BlinkWebProcessManager::getWebProcessProfiling()
     return std::move(reply);
 }
 
-void BlinkWebProcessManager::deleteStorageData(const QString& identifier)
+void BlinkWebProcessManager::deleteStorageData(const std::string& identifier)
 {
     std::list<const WebAppBase*> runningAppList = runningApps();
     if (!runningAppList.empty()) {
@@ -100,7 +99,7 @@ void BlinkWebProcessManager::deleteStorageData(const QString& identifier)
 
     BlinkWebView* webview = new BlinkWebView();
     if (webview) {
-        webview->DeleteWebStorages(identifier.toStdString());
+        webview->DeleteWebStorages(identifier);
         delete webview;
     }
 }