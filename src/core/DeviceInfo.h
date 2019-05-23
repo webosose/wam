nickdiego/webosose-wam

[agl][qtless] QString-less WebAppManager: SystemLanguage and DeviceInfo
This patch removes QString usage from SystemLanguage and DeviceInfo
API's in WebAppManager, WebAppManagerService, DeviceInfo, WebPageBase
and WebPageBlink.

[SPEC-1911] Qt-less WAM: replace qt basic data structures

Signed-off-by: Nick Diego Yamane <nickdiego@igalia.com>

src/core/DeviceInfo.h
@@ -18,8 +18,7 @@
 #define DEVICEINFO_H
 
 #include <unordered_map>
-
-#include <QString>
+#include <string>
 
 class DeviceInfo {
 public:
@@ -31,14 +30,14 @@ public:
     virtual bool getDisplayHeight(int& value);
     virtual void setDisplayHeight(int value);
 
-    virtual bool getSystemLanguage(QString& value);
-    virtual void setSystemLanguage(QString value);
+    virtual bool getSystemLanguage(std::string& value);
+    virtual void setSystemLanguage(const std::string& value);
 
-    virtual bool getDeviceInfo(QString name, QString& value);
-    virtual void setDeviceInfo(QString name, QString value);
+    virtual bool getDeviceInfo(const std::string& name, std::string& value);
+    virtual void setDeviceInfo(const std::string& name, const std::string& value);
 
 private:
-    std::unordered_map<QString, QString> m_deviceInfo;
+    std::unordered_map<std::string, std::string> m_deviceInfo;
 };
 
 #endif /* DEVICEINFO_H */

webosose/wam

src/core/DeviceInfo.h
@@ -18,8 +18,7 @@
 #define DEVICEINFO_H
 
 #include <map>
-
-#include <QString>
+#include <string>
 
 class DeviceInfo {
 public:
@@ -31,14 +30,14 @@ public:
     virtual bool getDisplayHeight(int& value);
     virtual void setDisplayHeight(int value);
 
-    virtual bool getSystemLanguage(QString& value);
-    virtual void setSystemLanguage(QString value);
+    virtual bool getSystemLanguage(std::string& value);
+    virtual void setSystemLanguage(const std::string& value);
 
-    virtual bool getDeviceInfo(QString name, QString& value);
-    virtual void setDeviceInfo(QString name, QString value);
+    virtual bool getDeviceInfo(const std::string& name, std::string& value);
+    virtual void setDeviceInfo(const std::string& name, const std::string& value);
 
 private:
-    std::map<QString, QString> m_deviceInfo;
+    std::map<std::string, std::string> m_deviceInfo;
 };
 
 #endif /* DEVICEINFO_H */