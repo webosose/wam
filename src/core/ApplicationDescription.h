nickdiego/webosose-wam

[agl][qtless] Migrate ApplicationDescription away from QString
This patch get rid of QString/QStringList usage from
ApplicationDescription implementation and some related APIs.

[SPEC-1911] Qt-less WAM: replace qt basic data structures

Signed-off-by: Nick Diego Yamane <nickdiego@igalia.com>

src/core/ApplicationDescription.h
@@ -19,9 +19,8 @@
 
 #include <memory>
 #include <unordered_map>
-
-#include <QString>
-#include <QStringList>
+#include <set>
+#include <string>
 
 class ApplicationDescription {
 public:
@@ -95,7 +94,7 @@ public:
         return m_enyoBundleVersion;
     }
 
-    const QStringList& supportedEnyoBundleVersions() const
+    const std::set<std::string>& supportedEnyoBundleVersions() const
     {
         return m_supportedEnyoBundleVersions;
     }
@@ -163,12 +162,12 @@ public:
         }
 
         bool allowAnonymous;
-        std::unordered_map<QString, int> layers;
+        std::unordered_map<std::string, int> layers;
     };
 
     struct WindowClientInfo {
-        QString layer;
-        QString hint;
+        std::string layer;
+        std::string hint;
     };
 
     struct WindowGroupInfo {
@@ -177,7 +176,7 @@ public:
         {
         }
 
-        QString name;
+        std::string name;
         bool isOwner;
     };
 
@@ -206,7 +205,7 @@ private:
     std::string m_containerJS;
     std::string m_containerCSS;
     std::string m_enyoBundleVersion;
-    QStringList m_supportedEnyoBundleVersions;
+    std::set<std::string> m_supportedEnyoBundleVersions;
     std::string m_enyoVersion;
     std::string m_version;
     std::string m_v8SnapshotPath;

webosose/wam

src/core/ApplicationDescription.h
@@ -19,9 +19,8 @@
 
 #include <memory>
 #include <map>
-
-#include <QString>
-#include <QStringList>
+#include <set>
+#include <string>
 
 class ApplicationDescription {
 public:
@@ -95,7 +94,7 @@ public:
         return m_enyoBundleVersion;
     }
 
-    const QStringList& supportedEnyoBundleVersions() const
+    const std::set<std::string>& supportedEnyoBundleVersions() const
     {
         return m_supportedEnyoBundleVersions;
     }
@@ -163,12 +162,12 @@ public:
         }
 
         bool allowAnonymous;
-        std::map<QString, int> layers;
+        std::map<std::string, int> layers;
     };
 
     struct WindowClientInfo {
-        QString layer;
-        QString hint;
+        std::string layer;
+        std::string hint;
     };
 
     struct WindowGroupInfo {
@@ -177,7 +176,7 @@ public:
         {
         }
 
-        QString name;
+        std::string name;
         bool isOwner;
     };
 
@@ -206,7 +205,7 @@ private:
     std::string m_containerJS;
     std::string m_containerCSS;
     std::string m_enyoBundleVersion;
-    QStringList m_supportedEnyoBundleVersions;
+    std::set<std::string> m_supportedEnyoBundleVersions;
     std::string m_enyoVersion;
     std::string m_version;
     std::string m_v8SnapshotPath;