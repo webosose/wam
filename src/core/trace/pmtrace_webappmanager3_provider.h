fb57343b5b195cf2abefacb353e0d3c9aa60b9de
sukil.hong <sukil.hong@lge.com>
2022-10-18 12:04:36 +0900

    [op][n_upsable][build] Fix compilation with lttng-ust >= 2.13
    :Release Notes:
    Fix compilation with lttng-ust >= 2.13
    
    :Detailed Notes:
    |  error: #include expects "FILENAME" or <FILENAME>
    |  1140 | #include LTTNG_UST_TRACEPOINT_INCLUDE
    |       |          ^~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    long deprecated TRACEPOINT_INCLUDE_FILE macro was
    finally removed in 2.13.5, see:
    https://github.com/lttng/lttng-ust/commit/41858e2b6e8
    https://github.com/lttng/lttng-ust/commit/d2966b4b0b2
    
    :Testing Performed:
    Build Test
    
    :QA Notes:
    N/A
    
    :Issues Addressed:
    [WRO-12611] LTTng tracing is not working
    
    Change-Id: I281838e6810e05fda6e6af552d0a776a36b9dc2e
    Reviewed-on: http://gpro.lge.com/c/webosose/wam/+/339982
    Reviewed-by: Commit Msg Checker <commit_msg@lge.com>
    Reviewed-by: Ban Word Checker <ban_word@lge.com>
    Reviewed-by: Youngsoo Choi <youngsoo.choi@lge.com>
    Tested-by: Youngsoo Choi <youngsoo.choi@lge.com>

--- src/core/trace/pmtrace_webappmanager3_provider.h
+++ src/core/trace/pmtrace_webappmanager3_provider.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2013-2018 LG Electronics, Inc.
+// Copyright (c) 2013-2022 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,8 +17,8 @@
 #undef TRACEPOINT_PROVIDER
 #define TRACEPOINT_PROVIDER pmtrace_webappmanager3
 
-#undef TRACEPOINT_INCLUDE_FILE
-#define TRACEPOINT_INCLUDE_FILE pmtrace_webappmanager3_provider.h
+#undef TRACEPOINT_INCLUDE
+#define TRACEPOINT_INCLUDE "pmtrace_webappmanager3_provider.h"
 
 #ifdef __cplusplus
 extern "C" {
